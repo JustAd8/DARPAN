24e175e190323b8bd00b650e0c1012739ac3c77b
Umesh-Manna <ume.man.rt24@dypatil.edu>
2026-03-03 19:48:58 +0530

    added spring boot backend

---
+++ backend/src/test/java/com/telemetry/backend/TelemetryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemetry.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemetryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
