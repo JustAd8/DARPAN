cb8ef33c56fd4199c4a39d289512629ded7f7d01
Umesh-Manna <ume.man.rt24@dypatil.edu>
2026-03-04 09:56:51 +0530

    1. Added JSON parsing logic (TelemetryMessage.java), 2. Added code to handle Websocket endpoints (WebSocketConfig.java), 3. Added core logic code for telemetry processing (TelemetryProcessor.java) :

---
+++ backend/src/main/java/com/umesh/telemetry/TelemetryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umesh.telemetry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemetryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelemetryBackendApplication.class, args);
+	}
+
+}
